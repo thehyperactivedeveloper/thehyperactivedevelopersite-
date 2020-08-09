fb74286a7597946ad7d8b14e1b54b6fa36523057
kynda <kyndaloyola@gmail.com>
2020-08-09 14:51:36 +0100

    more changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'thehyperactivedeveloper', content: 'Portafolio' },
+              { name: '--', content: '--' },
             ]}
           >
             <html lang="en" />
